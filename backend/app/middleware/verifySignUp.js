66d6bd763d17c8e323602a0178e616024b85f87e
Oleksandr Yarotskyi <uc2020242399@student.uc.pt>
2025-12-10 21:19:47 +0000

    BACKEND WORK - DB & Auth Finished

--- backend/app/middleware/verifySignUp.js
+++ backend/app/middleware/verifySignUp.js
@@ -7,14 +7,22 @@ const ROLES = ["user", "admin"];
 const checkDuplicateUsernameOrEmail = async (req, res, next) => {
     const username = req.body.username;
     const email = req.body.email;
+    const pass = req.body.password;
 
     try {
+        if (!username || !email || !pass) {
+            return res.status(400).json({ message: "Username, Email, and Password are required!" });
+        }
+
         const userByUsername = await query("SELECT id FROM users WHERE username = $1", [username]);
         const userByEmail = await query("SELECT id FROM users WHERE email = $1", [email]);
+
         if (userByUsername.rows.length > 0 || userByEmail.rows.length > 0) {
             return res.status(400).json({ message: "Failed! Username or Email is already in use!" });
         }
+
         next();
+        
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: "Unable to validate username/email!" });
