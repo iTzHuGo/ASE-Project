ae17e44afb636d344c40830d4ab1892c1c364acc
expo98 <miguelmpereira0409@gmail.com>
2025-12-14 03:27:48 +0000

    Creates routes for recommendation via ExpressJS

--- backend/app/routes/recommendation.routes.js
+++ backend/app/routes/recommendation.routes.js
@@ -1,11 +1,29 @@
 // app/routes/recommendation.routes.js
 import express from "express";
-import { getRatedMoviesByUser } from "../controllers/recommendation.controller.js";
+import { getRatedMoviesByUser, getRecommendationsForMovie, getRecommendationsForUser } from "../controllers/recommendation.controller.js";
 
 
 const router = express.Router();
  
- 
+// ---- USER RATINGS ROUTE
+// this is a helper rout for flask to get the movies rated by a USER
+// its simple this way instead of sending them on the recomendation route for user ID
 router.get("/user/:userId", getRatedMoviesByUser);
- 
+
+
+// ---- RECOMENDATION ROUTES ----
+
+
+// GET /api/recommendation/movie?title=Inception
+// Uses QUERY PAREMETER for the movie title
+// Calls the flask api recommendation based on a movie
+router.get("/recommendation/movie/", getRecommendationsForMovie)
+
+
+// GET /api/recommendation/movie?title=Inception
+// Uses PATH VARIABLE for the user Id
+// Calls the flask api for recommendation based on user rated movies
+router.get("/recommendation/user/:userId", getRecommendationsForUser)
+
+
 export default router;
